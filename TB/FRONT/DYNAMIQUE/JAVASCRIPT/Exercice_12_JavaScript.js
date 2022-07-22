1a1c439118b49c09b4870c03332e192249e887d8
PikkFitz <108813831+PikkFitz@users.noreply.github.com>
2022-07-22 20:46:18 +0200

    22/07/2022

--- TB/FRONT/DYNAMIQUE/JAVASCRIPT/Exercice_12_JavaScript.js
+++ TB/FRONT/DYNAMIQUE/JAVASCRIPT/Exercice_12_JavaScript.js
@@ -39,21 +39,21 @@ function exercice_12_js(){
     
         transformation_str1 = transformation_str1 + tab[i];
         
-    console.log("transformation_str1 : " + transformation_str1);
-    window.alert("transformation_str1 : " + transformation_str1);
+        console.log("transformation_str1 : " + transformation_str1);
+        window.alert("transformation_str1 : " + transformation_str1);
     
-    const tab_str1 = transformation_str1.split(str2);
-    console.log("Tableau str1 : " + tab_str1);
-    window.alert("Tableau str1 : " + tab_str1);
+        const tab_str1 = transformation_str1.split(str2);
+        console.log("Tableau str1 : " + tab_str1);
+        window.alert("Tableau str1 : " + tab_str1);
     
     
-    if ((n>tab_str1.length) || (n<1))
-    {
+        if ((n>tab_str1.length) || (n<1) || (n == null))
+        {
     
-        console.log(" Erreur : n° du mot hors du tableau ");
-        window.alert(" Erreur : n° du mot hors du tableau ");
+            console.log(" Erreur : n° du mot hors du tableau ");
+            window.alert(" Erreur : n° du mot hors du tableau ");
     
-    }
+        }
     
         console.log("Le mot n° " + n + " est : " + tab_str1[n-1]);
         window.alert("Le mot n° " + n + " est : " + tab_str1[n-1]);
@@ -61,7 +61,21 @@ function exercice_12_js(){
     }
     
 str1 = window.prompt("Entrer une liste de mots et/ou nombres : "); 
+
+    if ((str1 == 0) || (str1 == null) || (str1 == ""))
+        {
+            console.log("Mauvaise valeur");
+            alert("Mauvaise valeur");  
+        }
+
 str2 = window.prompt("Entrer un charactère 'séparateur' : ");
+
+    if ((str1 == null))
+    {
+        console.log("Mauvaise valeur");
+        alert("Mauvaise valeur");  
+    }
+
 n = window.prompt("Entrer le n° du mot désiré : ")
 
 strtok(str1, str2, n)
@@ -69,5 +83,5 @@ strtok(str1, str2, n)
     
 }
     
-    // Pierre Paul Jacques Oliver Laurent Martin Eric Maxime Nicolas Robert
-    // 1 2 3 4 5 6 7 8 9 10
+// Pierre Paul Jacques Oliver Laurent Martin Eric Maxime Nicolas Robert
+// 1 2 3 4 5 6 7 8 9 10
